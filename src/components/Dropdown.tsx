19bccc4934178d955965eef30ad391f6dfdb0245
rajeka <rekanayake@yahoo.com>
2024-12-18 16:25:12 -0800

    save expense completed

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,46 @@
+interface Props {
+  options: string[];
+  id: string;
+  name: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  onBlur: (e: React.FocusEvent<HTMLSelectElement>) => void;
+  label: string;
+  error?: string;
+  touched?: boolean;
+}
+const Dropdown = ({
+  options,
+  id,
+  name,
+  value,
+  onChange,
+  onBlur,
+  label,
+  error,
+  touched,
+}: Props) => {
+  return (
+    <div className="mb-3">
+      <label htmlFor={id} className="form-label">
+        {label}
+      </label>
+      <select
+        className="form-control"
+        id={id}
+        name={name}
+        onChange={onChange}
+        onBlur={onBlur}
+      >
+        <option value={value} label={`Select ${label}`} />
+        {options.map((option) => (
+          <option key={option} value={option} label={option} />
+        ))}
+      </select>
+      {touched && error ? (
+        <div className="text-danger fst-italic">{error}</div>
+      ) : null}
+    </div>
+  );
+};
+export default Dropdown;
---
+++ src/validation/expenseValidationSchema.ts
@@ -0,0 +1,15 @@
+import * as Yup from "yup";
+
+const expenseValidationSchema = () => {
+  Yup.object({
+    name: Yup.string()
+      .required("Name is required")
+      .min(3, "Name must be at least 3 characters"),
+    amount: Yup.number()
+      .required("Amount is required")
+      .positive("Amount must be positive"),
+    date: Yup.date().required("Date is required"),
+    category: Yup.string().required("Category is required"),
+  });
+};
+export default expenseValidationSchema;
